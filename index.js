f1beb10303309484ec474200794ecfbb601fd20e
SergioEspadas-Ksquare <Sergio.Espadas@itksquare.edu.mx>
2023-04-22 00:47:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 // 🌕 🌖 🌗 🌘 🌑 🌒 🌓 🌔 <-- Emojis are just text (Unicode). Neat, right? ;).
 
 // Remember: only imports from node's core libraries. For ex. node:fs
+
+import { readFile } from "fs";
+
+readFile("assets.json", (err, data) => {
+  //this will read the assets file
+  if (err) {
+    console.error(err);
+    process.exit(1); //1 - Uncaught Fatal Exception: There was an uncaught exception, and it was not handled by a domain or an uncaughtException event handler.
+  }
+
+  const assets = JSON.parse(data); //parses a JSON string
+
+  let idx = 0;
+
+  setInterval(() => {
+    const moonPhases = Object.values(assets); //get the values of assets.json
+    console.log(moonPhases[idx]); //logs the phases
+    idx++; //increase the counter
+
+    if (idx >= moonPhases.length) {
+      //restart the counter
+      idx = 0;
+    }
+  }, 1000); //every second increase
+});
