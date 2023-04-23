a8931c6cae3738d2ebee1be2a6cf8cb2b0e838de
Andrei Santos <Andrei.Santos@itksquare.edu.mx>
2023-04-23 14:28:14 -0600

    AndreiSantos moon-phase

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 // 🌕 🌖 🌗 🌘 🌑 🌒 🌓 🌔 <-- Emojis are just text (Unicode). Neat, right? ;).
+import * as fs from "fs";
 
+function getEmoji(moons) {
+  let moon = 1;
+  setInterval(() => {
+    console.log(moons[`m-${moon}`]);
+    moon = moon > 7 ? 1 : moon + 1;
+  }, 1000);
+}
+
+fs.readFile("assets.json", (error, data) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const datos = JSON.parse(data);
+  getEmoji(datos);
+});
 // Remember: only imports from node's core libraries. For ex. node:fs
